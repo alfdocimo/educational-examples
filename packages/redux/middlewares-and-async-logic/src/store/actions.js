cf63b3d3f94199271202ea92daba5587bd8faccc
Alfredo Narvaez <a.narvaez@assemblerschool.com>
2020-11-01 12:40:57 +0100

    feat: initialize repo with redux

---
+++ packages/redux/middlewares-and-async-logic/src/store/actions.js
@@ -0,0 +1,42 @@
+const Types = {
+  FETCH_USERS_STARTED: "FETCH_USERS_STARTED",
+  FETCH_USERS_SUCCESSFUL: "FETCH_USERS_SUCCESSFUL",
+  FETCH_USERS_ERROR: "FETCH_USERS_ERROR",
+};
+
+const fetchUsersSuccessful = (users) => ({
+  type: Types.FETCH_USERS_SUCCESSFUL,
+  payload: {
+    users,
+    status: "successful ✅",
+  },
+});
+
+const fetchUsersError = () => ({
+  type: Types.FETCH_USERS_ERROR,
+  payload: {
+    status: "error ❌",
+  },
+});
+
+const fetchUsersStarted = () => ({
+  type: Types.FETCH_USERS_STARTED,
+  payload: {
+    status: "loading... ⌛",
+  },
+});
+
+const fetchUsersInit = () => {
+  return async function (dispatch) {
+    dispatch(fetchUsersStarted());
+
+    return fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then(
+        (users) => dispatch(fetchUsersSuccessful(users)),
+        () => dispatch(fetchUsersError())
+      );
+  };
+};
+
+export { fetchUsersInit as fetchInit, Types };
